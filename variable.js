38cd1ffa400e4872c63cb3d0a0bc72ebdffbb866
refat374 <mdarafatkhandakar374@gmail.com>
2024-11-18 01:06:46 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,56 @@
+var number = 56;
+var name = "Tisha"
+var price = 100;
+var isDeath = false;
+var PIE = 3.1416;
+var friendName = "Emran";
+friendName = "Sakib";
+var productPrice = 99.99;
+productPrice = 100;
+
+// console.log(typeof (isDeath));
+
+
+// console.log(productPrice);
+
+var milkPrice = 11;
+var oilPrice = 160;
+
+//  var totalPrice = milkPrice + oilPrice;
+//  var totalPrice = oilPrice - milkPrice;
+var totalPrice = oilPrice / milkPrice;
+
+
+var banana = 10;
+// banana = 12;
+// banana = 10 + 3;
+// banana = banana + 4;
+// banana += 4;
+banana -= 3;
+// console.log(banana);
+
+var age = 20;
+age = + 2;
+age += 1;
+age++
+age = - 2;
+// console.log(age);
+
+var sweet = 50;
+var milk = "100.45";
+totalPrice = milk + sweet;
+// var convertNumber = parseInt(milk);
+var convertNumber = parseFloat(milk);
+var totalPrice = sweet + convertNumber;
+// consol e.log(totalPrice);
+
+
+var today = new Date();
+// console.log(typeof (today));
+
+var first = 0.1;
+var second = 0.2;
+var subTotal = first + second;
+// console.log(subTotal.toFixed(3));
+// console.log(subTotal.toFixed(1));
+console.log(parseFloat(subTotal.toFixed(1)));
