d384965dabf6ba59644d979a53fd9a86dd6d97d6
refat374 <mdarafatkhandakar374@gmail.com>
2024-11-20 21:11:42 +0600

    commit

---
+++ array.js
@@ -0,0 +1,92 @@
+var student = ['Imran', 'humayan', 'sadman', 'salim'];
+
+//array lenth//
+
+// console.log(student.length);
+var item = ['alu', 'potol', 'begun', 'peyaj', 'roshun'];
+// console.log(item.length);
+// console.log(Mix.length);
+
+//array indexof//
+
+// const MixArrayIndex = Mix.indexOf(5);
+// console.log(MixArrayIndex);
+
+// var findFixIndex = Mix[9];
+// console.log(findFixIndex);
+
+//How to push an element//
+
+
+var Mix = ['arafat', 16, true, 'refat', 'student', 67787565567];
+// console.log(Mix);
+// Mix[2] = 'false';
+// Mix.push('Imran')
+
+//how to push ali before arafat//
+//if (index !== -1) {
+// mix.splice(index, 0, 'sahaf');
+//}
+//how to cut an element//
+
+// Mix.pop() 
+// Mix.pop()
+// Mix.pop()
+// Mix.pop()
+// Mix.pop()
+// Mix.pop()
+
+//how to cut fist element//
+
+// Mix.shift()
+// console.log(Mix);
+
+//how to compere an element//
+
+// console.log(5 > 6);
+// console.log(5 < 6);
+// console.log(5 == 6);
+// console.log(5 == 5);
+// console.log(5 != 6);
+// console.log(5 != 5);
+
+var myLove = 99;
+var herLove = 100;
+
+// console.log(myLove > herLove);
+// console.log(myLove == herLove);
+// console.log(myLove < herLove);
+// console.log(myLove != herLove);
+
+//goodStudy && goodPerson
+// hasMony 
+
+// is food ready == true // khabo
+// var isFoodReady = true;
+// if (isFoodReady == true) {
+//     console.log('ami khabar khabo');
+
+// }
+
+var iPhone = 160000;
+var myBudget = 90000;
+// if (iPhone < myBudget) {
+//     console.log('iPhone kine felbo');
+
+// }
+// else (iPhone > myBudget) {
+//     console.log('iPhone kine felbo abong futani marabo');
+
+// }
+// if (iPhone < myBudget) {
+//     console.log('iPhone kine felbo');
+
+// }
+// else {
+//     console.log('bosen bosen');
+
+// }
+
+var jobHolder = false;
+var saveMoney = 150000;
+var hasFlat = true;
